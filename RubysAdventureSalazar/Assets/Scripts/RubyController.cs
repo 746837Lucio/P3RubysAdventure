22cbed7a923b2e8e495b86d8a7dd01b15d9def91
746837Lucio <luciosalazar0503@gmail.com>
2023-11-02 11:38:09 -0700

    Added tiles and Decorations
    I made a 40x40 world with tiles and water tiles.

--- RubysAdventureSalazar/Assets/Scripts/RubyController.cs
+++ RubysAdventureSalazar/Assets/Scripts/RubyController.cs
@@ -10,8 +10,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      //QualitySettings.vSyncCount = 0;
-      //Application.targetFrameRate = 10;
+      
     }
 
     // Update is called once per frame
